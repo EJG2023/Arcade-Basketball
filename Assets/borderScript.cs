30576dd2f0b6ef5284c41cbefaee1642b53f43e7
EJG2023 <133731036+EJG2023@users.noreply.github.com>
2024-04-07 17:11:46 -0600

    borderScript.cs

--- Assets/borderScript.cs
+++ Assets/borderScript.cs
@@ -7,7 +7,7 @@ public class borderScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Disable the renderer component
+        // Disable the renderer component for the border above the machine so that it wont get stuck and make it invisible
         gameObject.GetComponent<Renderer>().enabled = false;
     }
 
